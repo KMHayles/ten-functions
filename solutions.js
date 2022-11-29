20a351ea54954ebb4a6039914ee03a2d6ecc7701
Kenneth Hayles <kenneth.m.hayles@gmail.com>
2022-11-29 14:02:33 -0600

    completed additional answers 1-8

--- solutions.js
+++ solutions.js
@@ -21,3 +21,116 @@
  * concat
  */
 
+function isTrue(input) {
+    return input === true
+}
+
+
+function isFalse(input){
+    return input === false;
+}
+
+
+// falsey values: empty strings, Nan, null, undefined, and 0;
+
+
+function not(input){
+    return !input;
+}
+
+// Define a function named addOne that takes in a single input. If the input is a number or a numeric string, return the value plus one.
+//
+// addOne(0)                    // 1
+// addOne(2)                    // 3
+// addOne(-5)                   // -4
+// addOne(5.789)                // 6.789
+// addOne(Infinity)             // Infinity
+// addOne("2")                  // 3
+// addOne("0")                  // 1
+// addOne("banana")             // NaN
+// addOne(true)                 // NaN
+// addOne(NaN)                  // NaN
+
+function addOne(input){
+    let number = parseFloat(input);
+    return number + 1;
+}
+
+// Define a function named isEven that takes in a single input. If the input is an even number or a string containing an even number, return true. Any other input should return false for the output.
+//
+// isEven(2)                   // true
+// isEven(-8)                  // true
+// isEven(0)                   // true
+// isEven("42")                // true
+// isEven(1)                   // false
+// isEven("-3")                // false
+// isEven(false)               // false
+// isEven("banana")            // false
+
+function isEven(input) {
+    if (typeof input === 'boolean'){
+        return false;
+    }
+    return input % 2 === 0;
+}
+// additional way
+// return parseFloat(input) % 2 === 0
+
+// Define a function named isIdentical that takes in two input arguments. If each input is equal both in data type and in value, then return true. If the values are not the same data type or not the same value, return false.
+
+// isIdentical(3, 3)                     // true
+// isIdentical(false, false)             // true
+// isIdentical("hello", "hello")         // true
+// isIdentical(3, 3.0)                   // true
+// isIdentical(undefined, undefined)     // true
+// isIdentical(2, "2")                   // false
+// isIdentical("javascript", "java")     // false
+
+
+
+function isIdentical(input1, input2){
+    return input1 === input2;
+}
+
+// Define a function named isEqual that takes in two input arguments. If each argument is equal only in value, then return true. Otherwise return false.
+//
+// isEqual(3, "3")                       // true
+// isEqual("abc123", "abc123")           // true
+// isEqual(true, 1)                      // true
+// isEqual(0, false)                     // true
+// isEqual(4, -5)                        // false
+// isEqual("java", "javascript")         // false
+
+
+function isEqual(input1, input2){
+    return input1 == input2;
+}
+
+
+// Define a function named or that takes in two input arguments. The output returned should be the result of an or operation on both inputs.
+//
+// or(true, true)                    // true
+// or(true, false)                   // true
+// or(false, true)                   // true
+// or(false, false)                  // false
+// or("hello", "world")              // "hello" (this behavior is non-obvious, research more!)
+
+function or(input1, input2) {
+    return input1 || input2;
+}
+
+// Define a function named and that takes in two input arguments and returns the result of a logical and operation of both inputs.
+//
+// and(true, true)                    // true
+// and(true, false)                   // false
+// and(false, true)                   // false
+// and(false, false)                  // false
+// and("hello", "world")              // "world" (this behavior is non-obvious, research more)
+
+function and(input1, input2) {
+    return ;
+}
+
+
+function concat(string1, string2){
+}
